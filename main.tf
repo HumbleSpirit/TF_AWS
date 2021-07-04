ab64ea8117f678343882888703834cb4dce9cdfb
HumbleSpirit <borodkin@gmail.com>
2021-07-04 21:48:34 +0300

    Added DynamoDB section

--- main.tf
+++ main.tf
@@ -26,3 +26,13 @@ resource "aws_instance" "Jenkins_Server" {
 resource "aws_s3_bucket" "bucket" {
   bucket = "terraform-state-sb"
 }
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-lock"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
